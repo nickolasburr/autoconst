7ea7e8272b53f100c82365890b64b2d3b693a293
Nickolas Burr <nickolasburr@gmail.com>
2024-04-16 00:33:18 -0400

    Add PackageInstallPlugin class

---
+++ src/Composer/Plugin/PackageInstallPlugin.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * PackageInstallPlugin.php
+ *
+ * @package     Autoconst\Composer\Plugin
+ * @copyright   Copyright (C) 2024 Nickolas Burr <nickolasburr@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace Autoconst\Composer\Plugin;
+
+use Autoconst\Composer\Installer\PackageQueueManager;
+use Composer\Composer;
+use Composer\EventDispatcher\Event;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Plugin\PluginInterface;
+
+use const PHP_INT_MAX;
+
+final class PackageInstallPlugin implements PluginInterface, EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(
+        Composer $composer,
+        IOInterface $io
+    ) {}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deactivate(
+        Composer $composer,
+        IOInterface $io
+    ) {}
+
+    /**
+     * @param Event $event
+     * @return void
+     */
+    public function install(Event $event)
+    {
+        /** @var PackageInterface $package */
+        $package = $event->getOperation()->getPackage();
+        PackageQueueManager::enqueue($package);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(
+        Composer $composer,
+        IOInterface $io
+    ) {}
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-package-install' => ['install', PHP_INT_MAX],
+            'post-package-update' => ['install', PHP_INT_MAX],
+        ];
+    }
+}
