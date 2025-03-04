243d76a2b375feef4d0c2d3b19bcde2fd396aec3
Nickolas Burr <nickolasburr@gmail.com>
2025-03-04 00:01:02 -0500

    Update autoconst.php

--- autoconst.php
+++ autoconst.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * autoload.php
+ * autoconst.php
  *
  * @package     Autoconst
- * @copyright   Copyright (C) 2024 Nickolas Burr <nickolasburr@gmail.com>
+ * @copyright   Copyright (C) 2025 Nickolas Burr <nickolasburr@gmail.com>
  */
 
-/*
+/**
  * Composer provides $file as an unresolved file path.
  * If the containing directory is a symlink, it needs
  * to be resolved to get the real path to vendor-dir.
@@ -23,7 +23,4 @@ $vendorDir = dirname($dirPath);
 
 /** @var string $constFile */
 $constFile = $vendorDir . '/composer/autoload_constants.php';
-
-if (file_exists($constFile)) {
-    require_once $constFile;
-}
+!file_exists($constFile) or require $constFile;
